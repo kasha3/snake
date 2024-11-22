0d84d060e31f7b0bb6c17980d4fe9079d7366d7f
FadAlex <fadeevsasha0906@gmail.com>
2024-11-22 17:45:38 +0500

    Модель представления данных пользователя. Фадеев.

---
+++ Common/ViewModelUserSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Common
+{
+    public class ViewModelUserSettings
+    {
+        public string IPAddress { get; set; }
+        public string Port { get; set; }
+        public string UserName { get; set; }
+        public int IdSnake = -1;
+    }
+}
