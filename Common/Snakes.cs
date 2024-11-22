bad363f39de5a68a74ea07dedeeb281480563292
FadAlex <fadeevsasha0906@gmail.com>
2024-11-22 17:41:07 +0500

    Созданный класс змеи. Фадеев.

---
+++ Common/Snakes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Snakes
+    {
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+            public Point(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+            public Point() { }
+        }
+        public enum Direction
+        {
+            Left, Right, Up, Down, Start
+        }
+
+        public List<Point> Points = new List<Point>();
+        public Direction direction = Direction.Start;
+        public bool GameOver = false;
+    }
+
+}
